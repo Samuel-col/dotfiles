0d670e205d38adcdb361caea4f9846ce6f586803
Samuel-col <68486038+Samuel-col@users.noreply.github.com>
2021-11-14 11:29:42 -0500

    Add files via upload

---
+++ startup.jl
@@ -0,0 +1,13 @@
+# Paquetes
+using LinearAlgebra
+using Statistics
+using StatsBase
+using MultivariateStats
+using Latexify
+# Saludo
+print("\e[1m\e[38;2;245;187;7;249m","#########################\n")
+print("\e[1m\e[38;2;245;187;7;249m","## ¡Bienvenido Samuel! ##\n")
+print("\e[1m\e[38;2;245;187;7;249m","#########################\n")
+# Ajustes
+Latexify.copy_to_clipboard(true)
+

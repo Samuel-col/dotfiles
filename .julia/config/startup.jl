415d09a4ee683eb5acbb378dc055b2a54e1df25a
Samuel-col <68486038+Samuel-col@users.noreply.github.com>
2022-06-25 00:41:28 -0500

    Add files via upload

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -6,10 +6,14 @@ using Statistics
 using StatsBase
 #using MultivariateStats
 using Latexify
+# using Term
 # Saludo
 print("\e[1m\e[38;2;245;187;7;249m","#########################\n")
 print("\e[1m\e[38;2;245;187;7;249m","## ¡Bienvenido Samuel! ##\n")
 print("\e[1m\e[38;2;245;187;7;249m","#########################\n")
+# print(@yellow "#########################\n")
+# print(@yellow "## ¡Bienvenido Samuel! ##\n")
+# print(@yellow "#########################\n")
 # Ajustes
 Latexify.copy_to_clipboard(true)
 
